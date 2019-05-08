60ea733bcc3e9787d10869961ea12bb2cf99f649
Michael Stark <mstark5652@gmail.com>
2019-05-07 19:58:21 -0500

    Add project files.

---
+++ NLP.Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NLP.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
